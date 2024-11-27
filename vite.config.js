b94ece5fad790737ae198d2a8158fb16cee12208
fred <fred.yah@mars-deg.com>
2024-11-27 13:12:32 +0800

    modified css

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/fredresume/', // 將 <your-repository-name> 替換為你的 GitHub 儲存庫名稱
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
 });
